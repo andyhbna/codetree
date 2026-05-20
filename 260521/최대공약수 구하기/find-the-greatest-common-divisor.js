a309a746ed51fc202bc305544e3de7afc5a6099a
Hyunbin Na <42274195+andyhbna@users.noreply.github.com>
2026-05-21 03:08:15 +0900

    [Passed] 최대공약수 구하기 (66ms, 10MB)

---
+++ 260521/최대공약수 구하기/find-the-greatest-common-divisor.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(0, "utf8").trim().split(" ").map(Number);
+
+const n = input[0];
+const m = input[1];
+
+function gcd(a, b) {
+  while (b !== 0) {
+    let r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
+}
+
+console.log(gcd(n, m));
