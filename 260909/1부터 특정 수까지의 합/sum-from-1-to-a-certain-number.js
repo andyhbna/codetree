eb024ce23b949ee362f6ff45e2e5d5fd4ce846fa
Hyunbin Na <42274195+andyhbna@users.noreply.github.com>
2026-09-09 15:01:39 +0900

    [Passed] 1부터 특정 수까지의 합 (72ms, 10MB)

---
+++ 260909/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+
+function solution(n) {
+    let sum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    return Math.floor(sum / 10);
+}
+
+console.log(solution(n));
